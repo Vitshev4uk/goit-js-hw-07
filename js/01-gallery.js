6b1c36e759cd76ec40028247276ce0d15c52e6fe
Vitshev4uk <116673473+Vitshev4uk@users.noreply.github.com>
2023-03-21 23:21:34 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,15 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+console.log(gallery);
+
+gallery.addEventListener('click', (event) => {
+    console.log('click on gallery')
+})
+
+const image = galleryItems.map(item => {
+    
+
+});
