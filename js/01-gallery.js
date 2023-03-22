bb33094193c5c0cc51d2c0a101bb28b89dabf73d
Vitshev4uk <116673473+Vitshev4uk@users.noreply.github.com>
2023-03-22 11:50:12 +0100

    update_gallery1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,11 +6,22 @@ console.log(galleryItems);
 const gallery = document.querySelector('.gallery');
 console.log(gallery);
 
-gallery.addEventListener('click', (event) => {
-    console.log('click on gallery')
-})
+const markup = galleryItems.map(item => {
+ return `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}";
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+});
+console.log(markup);
+gallery.innerHTML = markup.join('');
 
-const image = galleryItems.map(item => {
-    
+gallery.addEventListener('click', (event) => {
+    event.preventDefault();
 
 });
